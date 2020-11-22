8ef180cc8e746a060d454e9b018cce0237ee6f45
Lukas-Poslusny <48207694+Lukas-Poslusny@users.noreply.github.com>
2020-11-22 17:48:59 +0100

    Revert "Revert "slight inspiration x)""
    This reverts commit 131e2556bfe11cd59ad13f2c5bd90ae1c910e00c.

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {FormsModule} from '@angular/forms';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
